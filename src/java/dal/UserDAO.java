15dd9ed502c40b9019a0dad60dacad302529de12
hunghqhe180380-hub <hunghqhe180380@fpt.edu.vn>
2026-01-21 15:07:19 +0700

    "Update Login by Email or UserName"

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -19,22 +19,34 @@ public class UserDAO extends DBContext {
     // register => confirm email
     // login => only allow login if email is confirmed 
     //login
-    public boolean isExistAccount(String email, String password) {
+    public boolean isExistAccount(String name, String password) {
+        String query = "";
+        //verify user login via Email or UserName
+        if (name.contains("@")) {
+            query = "where [Email] = ?";
+        } else {
+            query = "where [UserName] = ?";
+        }
         try {
-            String sql = "select  [Email], [PasswordHash]\n"
-                    + "from [dbo].[AspNetUsers]\n"
-                    + "where [Email] = ? ";
+            String sql = "select  [Email], [UserName], [PasswordHash] "
+                    + "  from [dbo].[AspNetUsers]"
+                    + query;
             statement = connection.prepareStatement(sql);
-            statement.setObject(1, email);
+            statement.setObject(1, name);
             resultSet = statement.executeQuery();
+            //validation Email/UserName and Password to allow login
             while (resultSet.next()) {
-                if (resultSet.getObject("Email").equals(email)) {
-                    return true;
+                if (resultSet.getObject("Email").equals(name) || resultSet.getObject("UserName").equals(name)) {
+                    if(resultSet.getObject("PasswordHash").equals(password)){
+                        //if correct information => allow to login
+                        return true;
+                    }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //if not correct information => not allow to login
         return false;
     }
 }
