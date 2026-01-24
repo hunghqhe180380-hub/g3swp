078e1ffbc2d0fde95466b6d709d147dfbd5a07f6
hunghqhe180380-hub <hunghqhe180380@fpt.edu.vn>
2026-01-25 04:14:21 +0700

    Create User.java

---
+++ src/java/model/User.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hung2
+ */
+public class User {
+    private String userID;
+    private String role;
+    private String userName;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userID, String role) {
+        this.userID = userID;
+        this.role = role;
+    }
+
+    public User(String userID, String role, String userName, String fullName) {
+        this.userID = userID;
+        this.role = role;
+        this.userName = userName;
+        this.fullName = fullName;
+    }
+
+    public User(String userID, String role, String fullName) {
+        this.userID = userID;
+        this.role = role;
+        this.fullName = fullName;
+    }
+
+    
+    
+    //constructor to create new account
+
+    public User(String userID, String role, String userName, String fullName, String email, String phoneNumber, String password) {
+        this.userID = userID;
+        this.role = role;
+        this.userName = userName;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
