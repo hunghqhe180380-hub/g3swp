48eef8ee572abda7dbb6b3fb048b519518e5cea5
hunghqhe180380-hub <hunghqhe180380@fpt.edu.vn>
2026-01-26 02:10:15 +0700

    Create Message.java

---
+++ src/java/message/Message.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package message;
+
+/**
+ *
+ * @author hung2
+ */
+public class Message {
+    public static final String MSG01 = "Email or User name not all null.";
+    public static final String MSG02 = "Invalid email format";
+    public static final String MSG03 = "Full name can only contain letters and spaces";
+    public static final String MSG04 = "Password not all null.";
+    public static final String MSG05 = "Incorrect username or password. Please check again.";
+    public static final String MSG06 = "Password has atleast 8 characters, 1 uppercase, 1 number.";
+    public static final String MSG07 = "The password and confirmation password do not match.";
+    public static final String MSG08 = "User Name must be between 4 and 20 characters.";
+    public static final String MSG09 = "Invalid Phone Number. Please enter 10 digits number start by 0 or + 84.";
+    public static final String MSG10 = "The password and confirmation password do not match.";
+}
